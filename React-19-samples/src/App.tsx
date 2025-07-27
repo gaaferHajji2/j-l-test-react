4cb6311bca0b3d687e0279a10c4f3a1274fdd4ad
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-27 20:31:27 +0300

    Adding type enum and create info && warning alert

--- React-19-samples/src/App.tsx
+++ React-19-samples/src/App.tsx
@@ -1,8 +1,21 @@
 import "./App.css";
-import Alert from "./components/Alert";
+import Alert, { AlertType } from "./components/Alert";
 
 function App() {
-  return <Alert msg="Something Happened" title="Oh no!!!"/>
+  return (
+    <>
+      <Alert
+        msg="Something Happened"
+        title="Oh no!!!"
+        type={AlertType.warning}
+      />
+      <Alert
+        msg="Msg Info Here"
+        title="Info"
+        type={AlertType.information}
+      />
+    </>
+  );
 }
 
 export default App;
--- React-19-samples/src/components/Alert.tsx
+++ React-19-samples/src/components/Alert.tsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import { AlertType } from "../constant";
+
 
 
 interface AlertData {
     title: string,
-    msg: string
+    msg: string,
+    type: AlertType 
 }
 
-const Alert = (props: AlertData) => {
+
+
+const Alert = ({title, msg, type = AlertType.information }: AlertData) => {
   return (
     <div>
         <div>
-            <span role='image' aria-label="Warning">⚠️⚠️⚠️</span>
-            <span>{props.title}</span>
+            <span role='image' aria-label={type == AlertType.warning ? "Warning" : "Information"}>
+              {type == AlertType.warning ? "⚠️⚠️⚠️" : "ℹ️ℹ️ℹ️"}
+            </span>
+            <span>{title}</span>
         </div>
 
         <div>
-            {props.msg}
+            {msg}
         </div>
     </div>
   )
