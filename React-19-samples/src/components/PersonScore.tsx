cd8af1e77b009fb9acd67d32107ce7158aa6b0af
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-10 19:54:04 +0300

    fix a bug about adding name as dependency, and add score state

--- React-19-samples/src/components/PersonScore.tsx
+++ React-19-samples/src/components/PersonScore.tsx
@@ -4,6 +4,7 @@ import { getPerson } from "../utils/Person";
 export function PersonScore () {
 
     const [name, SetName] = useState<string>("");
+    const [score, setScore] = useState<number>(0)
 
     useEffect(() =>  {
         getPerson().then(person => {
@@ -21,9 +22,30 @@ export function PersonScore () {
         return () => SetName("")
     }, [])
 
+    function addToScore() {
+        setScore((prev) => prev + 1);
+    }
+
+    function subtractFromScore() {
+        if(score!=0) {
+            setScore((prev) => prev-1);
+        }
+    }
+
+    function resetScore() {
+        setScore(0);
+    }
+
     if(name == "") {
         return <h1>Loading name...</h1>
     }
 
-    return (<h1>My name is: {name}</h1>);
+
+
+    return (<div>
+        <h1>My name is: {name}, Score: {score}</h1>
+        <button onClick={() => addToScore()}>Add</button>
+        <button onClick={() => subtractFromScore() }>Subtract</button>
+        <button onClick={() => resetScore() }>Reset</button>
+    </div>);
 }
