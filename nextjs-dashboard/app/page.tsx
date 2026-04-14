4b610f2cc235d8b77ac99457381df626ceddfa39
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-14 12:46:50 +0300

    Adding basic files & remove unused code

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -48,7 +48,7 @@ export default function Page() {
           />
         </div>
 
-        <InvoiceStatus status='pending' />
+        {/* <InvoiceStatus status='pending' /> */}
       </div>
     </main>
   );
