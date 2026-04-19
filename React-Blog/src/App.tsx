8103baefabb023f9f26893eed963e59c69bad780
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-19 23:50:43 +0300

    Init Main Content With Article01

--- React-Blog/src/App.tsx
+++ React-Blog/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/header'
+import MainContent from './components/main-content'
 
 function App() {
   return (
     <>
       <Header />
+      <MainContent />
     </>
   )
 }
---
+++ React-Blog/src/components/main-content.tsx
@@ -0,0 +1,13 @@
+import Article01 from "./article-1"
+
+function MainContent() {
+  return (
+    <div className="flex bg-blue-100 pb-4">
+        <main className="flex flex-col w-2/3 pl-6 pr-4 pt-4">
+            <Article01 />
+        </main>
+    </div>
+  )
+}
+
+export default MainContent
