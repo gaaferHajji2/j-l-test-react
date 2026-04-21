889677304dc8ea2c922c5953f833f13a161f0e14
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-21 09:23:21 +0300

    Build revenue chart with dashboard page

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
-import { lusitana } from "../ui/fonts"
+import { lusitana } from "@/app/ui/fonts"
+import { fetchRevenue } from "@/app/lib/data"
+import RevenueChart from "@/app/ui/dashboard/revenue-chart"
 
 export default async function Page() {
+
+    const revenue = await fetchRevenue()
+
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -13,7 +18,7 @@ export default async function Page() {
                 {/* <Card title="Total Customers" value={numberOfCustomers} type="customers" /> */}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                {/* <RevenueChart revenue={revenue}  /> */}
+                <RevenueChart revenue={revenue}  />
                 {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
             </div>
         </main>
