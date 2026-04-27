6e68d2903e1c65e995b148e0b8bf261cb139155d
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-27 13:47:49 +0300

    Updaing dashboard to use skeleton on revenue page only

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -3,12 +3,14 @@ import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "@/app/lib/data
 import RevenueChart from "@/app/ui/dashboard/revenue-chart"
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices"
 import { Card } from "@/app/ui/dashboard/cards"
+import { Suspense } from "react"
+import { RevenueChartSkeleton } from "@/app/ui/skeletons"
 
 export default async function Page() {
     // Fetch data in parallel
     // const data = await Promise.all([fetchRevenue(), fetchLatestInvoices(), fetchCardData()])
     // get the data of the dashboard page
-    const revenue = await fetchRevenue()
+    // const revenue = await fetchRevenue()
     const latestInvoices = await fetchLatestInvoices()
     const cardData = await fetchCardData()
     //Use previous data to collect statistics
@@ -29,7 +31,12 @@ export default async function Page() {
                 <Card title="Total Customers" value={numberOfCustomers} type="customers" />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <RevenueChart revenue={revenue}  />
+                {/* <RevenueChart revenue={revenue}  /> */}
+
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+
                 <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
