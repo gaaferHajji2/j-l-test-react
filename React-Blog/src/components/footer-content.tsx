30be155a17368a4f7229c5749b90d6144121abe9
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-28 23:50:26 +0300

    Add contact us component to footer

--- React-Blog/src/components/footer-content.tsx
+++ React-Blog/src/components/footer-content.tsx
@@ -1,10 +1,12 @@
 import AboutUsSection from "./about-us-section";
+import ContactUsSection from "./contact-us-section";
 
 export default function FooterContent() {
   return (
     <footer className="bg-blue-900">
         <div className="flex flex-wrap text-white p-3">
             <AboutUsSection />
+            <ContactUsSection />
         </div>
     </footer>
   )
