e72285f41d8b2daeef20e73e3ef020bed8bf716a
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-28 23:49:57 +0300

    Change style

--- React-Blog/src/components/about-us-section.tsx
+++ React-Blog/src/components/about-us-section.tsx
@@ -1,7 +1,7 @@
 
 export default function AboutUsSection() {
   return (
-    <div className="w-1/3 p-5 border-r border-blue-800 text-center">
+    <div className="w-1/3 p-3 border-r border-blue-800 text-center">
         <div className="my-6 text-xl font-semibold">ABOUT US</div>
         <p className="text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi fugit, odit ea soluta repellendus vel enim illo quae at, facilis rem modi magnam eius id.</p>
     </div>
