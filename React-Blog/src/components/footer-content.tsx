818e5fd7668ecb946369dcd56b627bae90f229cf
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-28 23:34:23 +0300

    Init the footer content

---
+++ React-Blog/src/components/footer-content.tsx
@@ -0,0 +1,10 @@
+
+export default function FooterContent() {
+  return (
+    <footer className="bg-blue-900">
+        <div className="flex flex-wrap text-white">
+            
+        </div>
+    </footer>
+  )
+}
