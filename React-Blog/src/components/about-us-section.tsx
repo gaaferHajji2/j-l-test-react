c585d0ffcbc174bf123dac019065e6d3ebe3c0ef
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-28 23:37:34 +0300

    Buildabout us component

---
+++ React-Blog/src/components/about-us-section.tsx
@@ -0,0 +1,9 @@
+
+export default function AboutUsSection() {
+  return (
+    <div className="w-1/3 p-5 border-r border-blue-800">
+        <div className="my-6 text-xl font-semibold">ABOUT US</div>
+        <p className="text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi fugit, odit ea soluta repellendus vel enim illo quae at, facilis rem modi magnam eius id.</p>
+    </div>
+  )
+}
