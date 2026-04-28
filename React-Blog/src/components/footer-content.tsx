ce584c711dd9f55bab8687a3aa0982dc88c0aadb
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-28 23:39:42 +0300

    Adding footer to app and change style

--- React-Blog/src/components/footer-content.tsx
+++ React-Blog/src/components/footer-content.tsx
@@ -1,9 +1,10 @@
+import AboutUsSection from "./about-us-section";
 
 export default function FooterContent() {
   return (
     <footer className="bg-blue-900">
-        <div className="flex flex-wrap text-white">
-            
+        <div className="flex flex-wrap text-white p-3">
+            <AboutUsSection />
         </div>
     </footer>
   )
