1303f031b35b0c62c2c2a25b6875bc28986d1224
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-04 14:32:01 +0300

    Change the element of invoice main page

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -4,7 +4,7 @@ import Search from "@/app/ui/search"
 
 
 export default function Page() {
-    return <p className="w-full">
+    return <div className="w-full">
         <div className="flex w-full items-center justify-between">
             <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
         </div>
@@ -22,5 +22,5 @@ export default function Page() {
         <div className="mt-5 flex w-full justify-center">
             {/* <Pagination totalPages={totalPages} /> */}
         </div>
-    </p>
+    </div>
 }
