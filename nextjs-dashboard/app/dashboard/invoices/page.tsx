9dbfd37158a12eb01fb5c6dea459a2528ce6aaf4
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-05 13:50:18 +0300

    Init pagination with total pages

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -3,7 +3,9 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons"
 import Search from "@/app/ui/search"
 import Table from '@/app/ui/invoices/table';
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { fetchInvoicesPages } from '@/app/lib/data';
 import { Suspense } from "react";
+import Pagination from "@/app/ui/invoices/pagination";
 
 export default async function Page(props: {
     searchParams?: Promise<{
@@ -15,6 +17,7 @@ export default async function Page(props: {
     const searchParams = await props.searchParams
     const query = searchParams?.query || ''
     const currentPage = parseInt(searchParams?.page || '1') || 1
+    const totalPages = await fetchInvoicesPages(query);
 
     return <div className="w-full">
         <div className="flex w-full items-center justify-between">
@@ -31,7 +34,7 @@ export default async function Page(props: {
         </Suspense> 
         
         <div className="mt-5 flex w-full justify-center">
-            {/* <Pagination totalPages={totalPages} /> */}
+            <Pagination totalPages={totalPages} />
         </div>
     </div>
 }
