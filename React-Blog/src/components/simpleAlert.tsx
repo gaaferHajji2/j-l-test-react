f85816c9cdd26178705e46c5ec32d84b32176d64
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-06 04:59:07 +0300

    Addding simple alert component using react

---
+++ React-Blog/src/components/simpleAlert.tsx
@@ -0,0 +1,9 @@
+
+export default function SimpleAlert() {
+  return (
+    <div className="flex flex-col p-4 pt-2 w-full border-blue-500 bg-blue-100">
+        <div className="font-semibold italic text-lg text-blue-500">Info</div>
+        <div className="leading-tight text-sm text-blue-800">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, est.</div>
+    </div>
+  )
+}
