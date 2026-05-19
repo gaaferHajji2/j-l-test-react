53cc69edef775586e881f49c54550e854506d980
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-19 05:20:22 +0300

    Complete the recipe card and change styles

--- React-Recipe-Card/src/componnets/RecipeCard.tsx
+++ React-Recipe-Card/src/componnets/RecipeCard.tsx
@@ -1,3 +1,7 @@
+import { faClock } from "@fortawesome/free-regular-svg-icons";
+import { faSignal } from "@fortawesome/free-solid-svg-icons";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons/faUtensils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface RecipeCardProps {
     imageurl: string;
@@ -12,12 +16,31 @@ interface RecipeCardProps {
 
 export default function RecipeCard(props: RecipeCardProps) {
   return (
-    <div className="m-5 shadow-md w-80 rounded overflow-hidden">
-        <img src={props.imageurl} alt={props.imgalt} />
+    <div className="m-5 shadow-md w-100 rounded-2xl overflow-hidden">
+        <img className="h-75 object-cover w-full" src={props.imageurl} alt={props.imgalt}/>
 
         <div className="p-2">
             <div className="font-bold text-lg text-gray-700 leading-snug">
                 <a href={props.titleurl} className="hover:underline">{props.title}</a>
+            </div>
+            
+            <p className="text-xs leading-tight tracking-wide my-2">{props.description}</p>
+
+            <div className="flex pt-2 border-t border-gray-300 text-sm text-gray-700 divide-x-2">
+                <div className="flex-1 text-center">
+                    <FontAwesomeIcon icon={faClock} />
+                    <p>{props.time}</p>
+                </div>
+
+                <div className="flex-1 text-center">
+                    <FontAwesomeIcon icon={faUtensils} />
+                    <p>{props.servings}</p>
+                </div>
+
+                <div className="flex-1 text-center">
+                    <FontAwesomeIcon icon={faSignal} />
+                    <p>{props.level}</p>
+                </div>
             </div>
         </div>
     </div>
