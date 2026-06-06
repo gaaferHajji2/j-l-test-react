efe7b68198856f68a029433840d299d93019d626
JafarLoka <gaafer.hajji1995@gmail.com>
2026-06-06 13:12:08 +0300

    Adding the theme context

---
+++ Event-Dashboard-UI/event-dashboard/src/context/ThemeContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState, useEffect, type ReactNode } from 'react';
+
+const ThemeContext = createContext({});
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within ThemeProvider');
+  }
+  return context;
+};
+
+export const ThemeProvider = ({ children }: { children: ReactNode}) => {
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme || 'light';
+  });
+
+  useEffect(() => {
+    document.documentElement.classList.remove('light', 'dark');
+    document.documentElement.classList.add(theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(prev => prev === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
