cc7226c722b704b584876a48f09a1bb924eccbd4
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-08-01 12:52:31 +0300

    Fix the issue of redirect to login with the auth token exists

--- Event-Dashboard-UI/event-dashboard/src/pages/LoginPage.tsx
+++ Event-Dashboard-UI/event-dashboard/src/pages/LoginPage.tsx
@@ -3,8 +3,20 @@ import ThemeToggle from '../components/Layout/ThemeToggle'
 import LanguageSwitcher from '../components/UI/LanguageSwitcher'
 import Logo from "../assets/Logo.jpg"
 import DarkLogo from "../assets/Logo_Dark.jpg"
+import { Navigate } from 'react-router-dom'
+
+const isAuthenticated = () => {
+  console.log("The Auth Token is: ", localStorage.getItem('authToken'))
+  return localStorage.getItem('authToken') !== null;
+};
+
 
 const LoginPage = () => {
+
+  if (isAuthenticated()) {
+    return <Navigate to="/dashboard" replace />
+  }
+
   return (
     <div className="min-h-screen flex bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
       {/* Top Right Controls */}
@@ -20,7 +32,7 @@ const LoginPage = () => {
 
       {/* Right Section - Image */}
       <div className="w-full lg:w-1/2 flex items-center justify-center">
-        <img src={Logo} className='dark:hidden'/>
+        <img src={Logo} className='dark:hidden' />
         <img src={DarkLogo} className='hidden dark:block' />
       </div>
     </div>
